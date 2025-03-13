f939e8e9b87ffdca5c8ed0d9d15c68c0d437ab9a
penhele <stephenhelk@gmail.com>
2025-03-13 23:37:48 +0700

    auto refresh 2

--- lib/screen/upload/add_story_screen.dart
+++ lib/screen/upload/add_story_screen.dart
@@ -164,7 +164,7 @@ class _AddStoryScreenState extends State<AddStoryScreen> {
     try {
       final result = await addStoryProvider.addStory(addStory);
       if (result) {
-        context.read<StoryListProvider>().refreshStoryList;
+        context.read<StoryListProvider>().refreshStoryList();
         widget.toHomeScreen();
       } else {
         scaffoldMessenger.showSnackBar(
