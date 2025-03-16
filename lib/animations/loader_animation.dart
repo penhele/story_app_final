799b088292e86cf878692e116d327b39581d3a8b
penhele <stephenhelk@gmail.com>
2025-03-16 14:40:41 +0700

    dart format . (kelupaan (lagi))

--- lib/animations/loader_animation.dart
+++ lib/animations/loader_animation.dart
@@ -8,26 +8,32 @@ class LoaderAnimation extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
-    Paint circlePaint = Paint()
-      ..shader = RadialGradient(
-        colors: [Colors.grey.shade400, Colors.grey.shade200],
-      ).createShader(Rect.fromCircle(
-        center: Offset(size.width / 2, size.height / 2),
-        radius: size.width / 3,
-      ))
-      ..style = PaintingStyle.fill;
+    Paint circlePaint =
+        Paint()
+          ..shader = RadialGradient(
+            colors: [Colors.grey.shade400, Colors.grey.shade200],
+          ).createShader(
+            Rect.fromCircle(
+              center: Offset(size.width / 2, size.height / 2),
+              radius: size.width / 3,
+            ),
+          )
+          ..style = PaintingStyle.fill;
 
     Offset center = Offset(size.width / 2, size.height / 2);
     canvas.drawCircle(center, size.width / 3, circlePaint);
 
-    Paint arcPaint = Paint()
-      ..shader = SweepGradient(
-        colors: [Color(0xff034C53), Color(0xff007074), Color(0xff57B4BA)],
-        stops: [0.0, 0.5, 1.0],
-      ).createShader(Rect.fromCircle(center: center, radius: size.width / 2))
-      ..style = PaintingStyle.stroke
-      ..strokeWidth = 8.0
-      ..strokeCap = StrokeCap.round;
+    Paint arcPaint =
+        Paint()
+          ..shader = SweepGradient(
+            colors: [Color(0xff034C53), Color(0xff007074), Color(0xff57B4BA)],
+            stops: [0.0, 0.5, 1.0],
+          ).createShader(
+            Rect.fromCircle(center: center, radius: size.width / 2),
+          )
+          ..style = PaintingStyle.stroke
+          ..strokeWidth = 8.0
+          ..strokeCap = StrokeCap.round;
 
     double startAngle = -math.pi / 2;
     double sweepAngle = 2 * math.pi * progress;
